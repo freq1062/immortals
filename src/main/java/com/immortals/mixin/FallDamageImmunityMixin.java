f7852b616b7d53df000c5cedfcea001257838243
Alex Min <alexminth@gmail.com>
2025-04-29 16:03:56 -0400

    mortal lifesteal

---
+++ src/main/java/com/immortals/mixin/FallDamageImmunityMixin.java
@@ -0,0 +1,29 @@
+package com.immortals.mixin;
+
+import com.immortals.AscensionUtils;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.entity.damage.DamageTypes;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(LivingEntity.class)
+public abstract class FallDamageImmunityMixin {
+    @Inject(method = "damage(Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/entity/damage/DamageSource;F)Z", at = @At("HEAD"), cancellable = true)
+    private void cancelFallDamage(ServerWorld world,
+            DamageSource source,
+            float amount,
+            CallbackInfoReturnable<Boolean> cir) {
+        if (source.isOf(DamageTypes.FALL)
+                && (LivingEntity) (Object) this instanceof ServerPlayerEntity player
+                && !world.isClient
+                && AscensionUtils.getAscended(player) == 1
+                && AscensionUtils.getCorruption(player) >= 1) {
+            cir.setReturnValue(false);
+        }
+    }
+}
